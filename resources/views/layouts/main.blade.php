c09f6ff4337c802c079ed45a0bb9ff40c7aa1ea8
tanut <Tonutjaidee@hotmail.com>
2021-04-05 02:22:12 +0700

    .

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -42,6 +42,23 @@
 
 
                             <li><a href="/course"><span class="text-light">Course</span></a></li>&nbsp  
+    <nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/student">RegSystem</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/student">Student</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/teacher">Teacher</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/course">Course</a>
+        </li>
        
       </ul>
       <div class="rounded float-end" style="width:100%; text-align:right;">
