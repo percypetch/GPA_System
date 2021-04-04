553650981be9980622188461d94050ec4508ff53
Kittipoom <percypetch2@gmail.com>
2021-04-05 02:21:54 +0700

    wqeqw

--- resources/views/course-update.blade.php
+++ resources/views/course-update.blade.php
@@ -31,76 +31,32 @@
 
     <form action="{{ route('course-update',['course' => $course->course_code,]) }}" method="post">
         @csrf
-
-        <table class="tablecenter normal alignRight">
-        <tr>
-            <td>
-                <span class=""><b>*Code</b></span>
-            </td>
-            <td>
-                <span class = "bluecolor">:: </span>
-            </td>
-            <td>
+        @csrf
+        <div class="input-group mb-3">
+            <span class="input-group-text" id="inputGroup-sizing-default">Code</span>
                 <input type="text" name="course_code" size="10" value="{{ old('course_code')?? $course->course_code }}" required>
-            </td>
-        </tr>
-
-        <tr>
-            <td>
-                <span class=""><b>*Name</b></span>
-            </td>
-            <td>
-                <span class = "bluecolor">:: </span>
-            </td>
-            <td>
-                <input type="text" name="course_name" size="50" value="{{ old('course_name')?? $course->course_name }}" required>
-            </td>
-        </tr>
-        <tr>
-            <td>
-                <span class=""><b>Credit</b></span>
-            </td>
-            <td>
-                <span class = "bluecolor">:: </span>
-            </td>
-            <td>
-                <select id="credit" name="credit">
-                @for($i=1 ; $i<=3 ; $i++)
-                    @if($course->credit ==  $i)
-                        <option value="{{ $i }}" selected>  
-                        {{ $i }}
-                        </option>
-                    @else
+                <input type="text" name="course_code" size="10" value="{{ old('course_code') }}" class="form-control" required>
+        </div>
+        <div class="input-group mb-3">
+            <span class="input-group-text" id="inputGroup-sizing-default">Name</span>
+            <input type="text" name="course_name" size="50" value="{{ old('course_name') }}" class="form-control" required>
+                </div>
+        <div class="input-group mb-3" style="width: 250px;">
+                <span class="input-group-text">Credit</span>
+                <select id="credit" name="credit" class="form-control" style="width:10px;">
+                    @for($i=1 ; $i<=3 ; $i++)
                         <option value="{{ $i }}">  
                         {{ $i }}
                         </option>
-                     @endif
-                @endfor
-                </select>
-            </td>
-        </tr>
-        <tr>
-            <td>
-                <span class=""><b>*Description</b></span>
-            </td>
-            <td>
-                <span class = "bluecolor">:: </span>
-            </td>
-            <td>
-                <textarea name="descriptions" id="" cols="30" rows="5">{{ old('descriptions')?? $course->descriptions }}</textarea>
-            </td>
-        </tr>
-        
-        <tr>
-            <td>
-            </td>
-            <td>
-            </td>
-            <td class="text-center">
-            <input type="submit">
-            </td>
-        </tr>
-        </table>
+                    @endfor
+                    </select>
+        </div>
+        <div class="form-floating">
+                <textarea name="descriptions"class="form-control" placeholder="Course descriptions." id="floatingTextarea2" style="height: 100px">{{ old('descriptions')?? $course->descriptions }}</textarea>
+                </div><br>
+        <div class="form-group row mb-0 justify-content-center">
+                 <button type="submit" class="btn btn-success">Submit</button>&nbsp
+        </div>
     </form>
     </main>
                     
